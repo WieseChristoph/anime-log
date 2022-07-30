39ee8e20b4eccbf7a93be2a65efcb981e0c18d2e
Christoph Wiese <wiese-christoph@gmx.de>
2022-07-31 01:09:04 +0200

    refactored redis file

--- lib/redis.ts
+++ lib/redis.ts
@@ -8,7 +8,7 @@ declare global {
 	var redis: Redis | undefined;
 }
 
-const redis =
+export const redis =
 	global.redis ||
 	new Redis(parseInt(process.env.REDIS_PORT), process.env.REDIS_HOST, {
 		password: process.env.REDIS_PASSWORD,
@@ -16,34 +16,15 @@ const redis =
 
 if (process.env.NODE_ENV !== "production") global.redis = redis;
 
-const connect = async () => {
-	const pingResult = await redis.ping();
-	if (pingResult === "PONG") return;
-	await redis.connect();
-};
-
-export const getCache = async (key: string) => {
-	await connect();
-	return redis.get(key);
-};
-
-export const setCache = async (key: string, value: string) => {
-	await connect();
-	await redis.set(key, value, "EX", TIMEOUT);
-};
-
 // Search for key in redis db and return value if found
 // if not found, get data via callback and store in redis db
 export const getOrSetCache = async (key: string, callback: () => any) => {
-	await connect();
 	return new Promise((resolve, reject) => {
 		redis.get(key, async (err, result) => {
 			if (err) reject(err);
 			else if (result) {
-				console.log("Cache hit");
 				resolve(JSON.parse(result));
 			} else {
-				console.log("Cache miss");
 				const freshData = await callback();
 				redis.set(key, JSON.stringify(freshData), "EX", TIMEOUT);
 				resolve(freshData);
