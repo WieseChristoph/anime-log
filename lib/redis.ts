69378bb6b38d64887d16760be4ec11cd7caa2ecd
Christoph Wiese <wiese-christoph@gmx.de>
2022-07-30 22:51:02 +0200

    switched from package "redis" to "ioredis"

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,22 +1,20 @@
-import { RedisClientType, createClient } from "redis";
+import Redis from "ioredis";
 
 const TIMEOUT = 60 * 60 * 24 * 7; // 1 week
 
 declare global {
 	// allow global `var` declarations
 	// eslint-disable-next-line no-var
-	var redis: RedisClientType | undefined;
+	var redis: Redis | undefined;
 }
 
 const redis =
 	global.redis ||
-	createClient({
-		socket: {
-			host: process.env.REDIS_HOST,
-			port: parseInt(process.env.REDIS_PORT),
-		},
+	new Redis(parseInt(process.env.REDIS_PORT), process.env.REDIS_HOST, {
 		password: process.env.REDIS_PASSWORD,
 	});
+
+if (process.env.NODE_ENV !== "production") global.redis = redis;
 
 const connect = async () => {
 	const pingResult = await redis.ping();
@@ -31,7 +29,7 @@ export const getCache = async (key: string) => {
 
 export const setCache = async (key: string, value: string) => {
 	await connect();
-	await redis.set(key, value, { EX: TIMEOUT });
+	await redis.set(key, value, "EX", TIMEOUT);
 };
 
 // Search for key in redis db and return value if found
@@ -39,21 +37,17 @@ export const setCache = async (key: string, value: string) => {
 export const getOrSetCache = async (key: string, callback: () => any) => {
 	await connect();
 	return new Promise((resolve, reject) => {
-		redis
-			.get(key)
-			.then(async (result) => {
-				if (result) {
-					console.log("Cache hit");
-					resolve(JSON.parse(result));
-				} else {
-					console.log("Cache miss");
-					const freshData = await callback();
-					redis.set(key, JSON.stringify(freshData), { EX: TIMEOUT });
-					resolve(freshData);
-				}
-			})
-			.catch(reject);
+		redis.get(key, async (err, result) => {
+			if (err) reject(err);
+			else if (result) {
+				console.log("Cache hit");
+				resolve(JSON.parse(result));
+			} else {
+				console.log("Cache miss");
+				const freshData = await callback();
+				redis.set(key, JSON.stringify(freshData), "EX", TIMEOUT);
+				resolve(freshData);
+			}
+		});
 	});
 };
-
-if (process.env.NODE_ENV !== "production") global.redis = redis;
